8a51f6bf12e28127ab57a2557a2d0f9a894a6f88
Jeff Blake <jtblake@middlebury.edu>
2025-03-01 16:00:20 -0500

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,25 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [
+		vitePreprocess(),
+		// sveltePreprocess(),
+	],
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
